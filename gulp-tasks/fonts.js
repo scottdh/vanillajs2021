c00404ffec0aceb335da8cbc5bceeac14ac4f5d0
Scott Herrington <scott.herrington@me.com>
2021-06-04 15:51:12 +0100

    added display=swap to google fonts

--- gulp-tasks/fonts.js
+++ gulp-tasks/fonts.js
@@ -11,7 +11,7 @@ const fonts = async () => {
 
   // Grabs fonts and CSS from google and puts in the dist folder
   const result = await instance.download(
-    "https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500"
+    "https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap&family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&display=swap"
   );
 
   return result;
