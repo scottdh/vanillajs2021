c7c43c4ea97942ed564569c5c9713126c873f861
Scott Herrington <scott.herrington@me.com>
2021-06-08 12:40:58 +0100

    removed console.log statement

--- src/js/character-count.js
+++ src/js/character-count.js
@@ -2,7 +2,6 @@ const textField = document.querySelector("#text");
 const characterCount = document.querySelector("#character-count");
 
 const updateCharacterCount = function (e) {
-  console.log("moo");
   characterCount.textContent = textField.value.length;
 };
 
