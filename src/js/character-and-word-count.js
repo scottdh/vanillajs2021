0ee621b218c0f34f37fa488da218c89556d75ae5
Scott Herrington <scott.herrington@me.com>
2021-06-09 10:47:22 +0100

    Replaced match with split because of bug

--- src/js/character-and-word-count.js
+++ src/js/character-and-word-count.js
@@ -4,10 +4,11 @@ const wordCount = document.querySelector("#word-count");
 
 const updateCharacterAndWordCount = function (e) {
   characterCount.textContent = textField.value.length;
-  wordCount.textContent = textField.value.match(/\S+/g).length;
 
-  // Split also works but need to reduce the index by 1
-  // wordCount.textContent = textField.value.split(/\S+/g).length - 1;
+  wordCount.textContent = textField.value.split(/\S+/g).length - 1;
+
+  // Match works but doesn't return word count to zero when deleting all text. Hmm
+  // wordCount.textContent = textField.value.match(/\S+/g || []).length;
 };
 
 textField.addEventListener("input", updateCharacterAndWordCount);
