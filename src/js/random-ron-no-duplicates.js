5680e9e89edd2c95bd2e815cb66180de11e092f9
Scott Herrington <scott.herrington@me.com>
2021-06-17 13:10:23 +0100

    completed random ron - no duplicates

---
+++ src/js/random-ron-no-duplicates.js
@@ -0,0 +1,41 @@
+const blockQuote = document.querySelector("blockquote");
+const getQuoteButton = document.querySelector("#get-quote");
+const quoteGeneratorAPI = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+let shownQuotes = [];
+
+const getNewQuote = function () {
+  fetch(quoteGeneratorAPI)
+    .then(function (response) {
+      // If the response is successful, get the JSON
+      if (response.ok) {
+        return response.json();
+      }
+      // Otherwise, throw an error
+      throw response.status;
+    })
+    .then(function (data) {
+      let quote = data[0];
+      if (shownQuotes.includes(quote)) {
+        getNewQuote();
+      } else {
+        shownQuotes.push(quote);
+        blockQuote.textContent = quote;
+      }
+      // After 50, empty the shownQuotes array
+      if (shownQuotes.length === 50) {
+        shownQuotes = [];
+      }
+    })
+    .catch(function (error) {
+      // There was an error
+      console.warn(error);
+      blockQuote.textContent = "Unable to load quote";
+    });
+};
+
+if ((blockQuote.textContent = "...")) {
+  getNewQuote();
+}
+
+getQuoteButton.addEventListener("click", getNewQuote);
---
+++ src/projects/random-ron-no-duplicates.md
@@ -0,0 +1,13 @@
+---
+title: "Random Ron - no duplicates"
+date: "2021-06-17"
+script: "random-ron-no-duplicates.js"
+---
+
+<figure class="flow" data-type="blockquote">
+  <blockquote aria-live="polite">...</blockquote>
+  <figcaption>
+    - Ron Swanson
+  </figcaption>
+</figure>
+<button id="get-quote">More Ron</button>
